ac3d169c3a7815e89b48c239728a2c611d9024d7
Jomar Milan <jomarm@jomarm.com>
2024-08-07 15:50:45 -0700

    Merge server and window

--- app.js
+++ app.js
@@ -1,5 +1,57 @@
-import { startServer } from './server.js';
-import { startApp } from './window.js';
+import getPort, { portNumbers } from 'get-port';
+import { WebSocketServer } from 'ws';
+import { BrowserWindow, app, ipcMain } from 'electron';
+import path from 'node:path';
+import crypto from "node:crypto";
 
-startServer();
-startApp();
+const port = await getPort({
+    port: portNumbers(49152, 65535)
+});
+const wss = new WebSocketServer({
+    port
+});
+console.log(`Server started on port ${ port }`);
+
+const key = await new Promise((resolve, reject) => {
+    crypto.generateKeyPair('ec', {
+        namedCurve: 'P-384'
+    }, (err, publicKey, privateKey) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve({ publicKey, privateKey });
+        }
+    })
+});
+
+wss.on('connection', (ws) => {
+    console.log('connection established');
+
+    ws.on('message', (data) => {
+        console.log(`recieved ${ data }`);
+    });
+});
+
+function createWindow() {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.resolve(import.meta.dirname, 'preload.js')
+        }
+    });
+
+    window.loadFile(path.resolve(import.meta.dirname, 'wwwdist/index.html'));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
